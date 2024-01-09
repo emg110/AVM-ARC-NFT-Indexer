c109d9212c7ec8ebb0c656c7a8fcef4003bbfbba
MG <emg110@gmail.com>
2024-01-09 22:40:36 +0300

    Fix

--- Arc74ModuleTokens.mjs
+++ Arc74ModuleTokens.mjs
@@ -242,7 +242,7 @@ export class Arc74ModuleTokens extends OpenAPIRoute {
         let statementInsert = ` SELECT * FROM arc72tokens ${where} ORDER BY round DESC ${limit? 'LIMIT ' + limit : 100} ${next? 'OFFSET ' + (next * limit) : ''}`;
         console.log('Statement: ', statementInsert)
         console.log('Binds: ', binds)
-        const { results } = await env.ARC_NFT_DB.prepare(statementInsert).bind(binds.split(',')).run();
+        const { results } = await env.ARC_NFT_DB.prepare(statementInsert).bind(binds.join(',')).run();
 
         let res = {
             results:  results || [],
